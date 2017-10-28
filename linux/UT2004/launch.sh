138b9e883ffbd6ac66803859a932e6bd09db84f3
ilumos <ilumos@gmail.com>
2017-10-28 03:12:02 +0100

    Removing user input from launch script

--- linux/UT2004/launch.sh
+++ linux/UT2004/launch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Please enter your password:"
-read -s UNREALADMINPASS
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR/server/System/ && ./ucc-bin server ONS-Torlan?game=Onslaught.ONSOnslaughtGame?AdminName=$USER?AdminPassword=$UNREALADMINPASS  -nohomedir ini=server-zph.ini
+cd $DIR/server/System/ && ./ucc-bin server ONS-Torlan?game=Onslaught.ONSOnslaughtGame?AdminName=$USER?AdminPassword=admin  -nohomedir ini=server-zph.ini
