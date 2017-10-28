1efc96015a66adcb7a9e7fb3382694b2c89db9dc
ilumos <ilumos@gmail.com>
2017-10-28 03:09:43 +0100

    Adding script dir variable into launchers

--- linux/QuakeIIIArena/launch.sh
+++ linux/QuakeIIIArena/launch.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR && cd server && ./q3ded +set fs_homepath "$SCRIPTPATH/server" +set fs_game osp +set dedicated 1 +set net_port 27960 +exec "server-zph.cfg"
+cd $DIR/server && ./q3ded +set fs_homepath "$DIR/server" +set fs_game osp +set dedicated 1 +set net_port 27960 +exec "server-zph.cfg"
