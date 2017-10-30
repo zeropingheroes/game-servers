44f9f84f5625756df3b9dbcf37be8e9b38a960d4
ilumos <ilumos@gmail.com>
2017-10-30 17:16:00 +0000

    Adding Steam username to all install scripts

--- linux/HalfLife2Deathmatch/update.sh
+++ linux/HalfLife2Deathmatch/update.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/../SteamCMD/steamcmd.sh +login anonymous +force_install_dir "$DIR/server" +app_update 232370 validate +quit
+$DIR/../SteamCMD/steamcmd.sh +login $STEAM_USERNAME +force_install_dir "$DIR/server" +app_update 232370 validate +quit
--- linux/Teeworlds/update.sh
+++ linux/Teeworlds/update.sh
@@ -2,6 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $DIR/../globals
-
 $DIR/../SteamCMD/steamcmd.sh +login $STEAM_USERNAME +force_install_dir "$DIR/server" +app_update 380840 validate +quit
