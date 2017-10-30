6abae128ee5a14598fb5abfb6d3f70d0de7a476d
ilumos <ilumos@gmail.com>
2017-10-30 21:36:41 +0000

    Fixing updaters

--- linux/TeamFortress2/update.sh
+++ linux/TeamFortress2/update.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/../SteamCMD/steamcmd.sh +login $STEAM_USERNAME +force_install_dir "$DIR/server" +app_license_request 4020 +app_update 232250 validate +quit
+$DIR/../SteamCMD/steamcmd.sh +login anonymous +force_install_dir "$DIR/server" +app_license_request 232250 +app_update 232250 validate +quit
