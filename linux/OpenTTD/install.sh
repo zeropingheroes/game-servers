02306d01b84296d9a069d8a218b6564a23206947
ilumos <ilumos@gmail.com>
2017-10-30 14:19:08 +0000

    Added OpenTTD configuration file and installer

--- linux/OpenTTD/install.sh
+++ linux/OpenTTD/install.sh
@@ -11,6 +11,17 @@ mkdir -p $DIR/server
 info "Downloading game server files"
 curl -L "https://binaries.openttd.org/releases/1.7.1/openttd-1.7.1-linux-generic-amd64.tar.gz" | tar -xvz -C "$DIR/server" --strip 1
 
-#info "Linking config files"
-#cp -rs $DIR/configs/* $DIR/server
+info "Downloading graphics set"
+curl -L "https://binaries.openttd.org/extra/opengfx/0.5.2/opengfx-0.5.2-all.zip" > "$DIR/server/opengfx-0.5.2-all.zip"
+
+info "Extracting graphics set"
+unzip -q "$DIR/server/opengfx-0.5.2-all.zip" -d "$DIR/server"
+tar -xf "$DIR/server/opengfx-0.5.2.tar" -C "$DIR/server/baseset" --strip 1
+
+info "Removing temporary files"
+rm "$DIR/server/opengfx-0.5.2-all.zip"
+rm "$DIR/server/opengfx-0.5.2.tar"
+
+info "Linking config files"
+cp -rs $DIR/configs/* $DIR/server
 
