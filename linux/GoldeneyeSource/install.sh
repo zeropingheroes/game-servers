377bed8cd9ebf4353f52a03b27a2142f12728782
ilumos <ilumos@gmail.com>
2017-10-30 17:28:57 +0000

    Removing sourcing from install scripts

--- linux/GoldeneyeSource/install.sh
+++ linux/GoldeneyeSource/install.sh
@@ -4,8 +4,6 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $DIR/../include
-
 info "Downloading game server files"
 
 curl -L "http://files.gameservermanagers.com/GoldenEyeSource/GoldenEye_Source_v5.0.1_full_server_linux.tar.bz2" | tar -xj -C "$DIR/server"
--- linux/Teeworlds/install.sh
+++ linux/Teeworlds/install.sh
@@ -4,11 +4,8 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $DIR/../include
-
 info "Downloading game server files"
 source $DIR/update.sh
 
 info "Linking config files"
 cp -rs $DIR/configs/* $DIR/server
-
