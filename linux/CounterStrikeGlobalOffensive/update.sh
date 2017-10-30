a8d7996fe5a661bdc3c155de070d3217685e36d8
ilumos <ilumos@gmail.com>
2017-10-30 20:08:03 +0000

    Adding subscription request to update scripts

--- linux/CounterStrikeGlobalOffensive/update.sh
+++ linux/CounterStrikeGlobalOffensive/update.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$DIR/../SteamCMD/steamcmd.sh +login $STEAM_USERNAME +force_install_dir "$DIR/server" +app_update 740 validate +quit
+$DIR/../SteamCMD/steamcmd.sh +login $STEAM_USERNAME +force_install_dir "$DIR/server" +app_license_request 4020 +app_update 740 validate +quit
