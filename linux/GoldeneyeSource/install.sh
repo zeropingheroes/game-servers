29a25d3370d923e86d3cfebd7f81096251fd0115
ilumos <ilumos@gmail.com>
2017-11-11 21:26:38 +0000

    Fixing install scripts to source instead of execute

--- linux/GoldeneyeSource/install.sh
+++ linux/GoldeneyeSource/install.sh
@@ -6,4 +6,4 @@ info "Downloading game server files"
 
 curl -L "http://files.gameservermanagers.com/GoldenEyeSource/GoldenEye_Source_v5.0.1_full_server_linux.tar.bz2" | tar -xj -C "$DIR/server"
 
-$DIR/update.sh
+source $DIR/update.sh
--- linux/HalfLife2Deathmatch/install.sh
+++ linux/HalfLife2Deathmatch/install.sh
@@ -3,5 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 info "Downloading game server files"
-
-$DIR/update.sh
+source $DIR/update.sh
