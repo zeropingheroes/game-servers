e047801739b62bb501c69015d7bb9f5d95c334ea
ilumos <ilumos@gmail.com>
2017-11-12 21:41:27 +0000

    Fixing config execution for Goldeneye Source server

--- linux/GoldeneyeSource/launchers/default.sh
+++ linux/GoldeneyeSource/launchers/default.sh
@@ -2,4 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR/../server && MALLOC_CHECK_=0 ./srcds_run -console -game ./gesource +exec server-zph.cfg +sv_lan 1 -ip 0.0.0.0 +map ge_facility_backzone +maxplayers 16
+# "-exec server-zph.cfg" does not work on command line
+# so instead add "exec" command to server.cfg
+rm "$DIR/server/gesource/cfg/server.cfg"
+echo "exec server-zph.cfg" > "$DIR/server/gesource/cfg/server.cfg"
+
+cd $DIR/../server && MALLOC_CHECK_=0 ./srcds_run -console -game ./gesource +sv_lan 1 +map ge_facility_backzone +maxplayers 16
