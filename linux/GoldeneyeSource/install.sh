faf824014de1ddecfb4629585621b433c6dc6065
ilumos <ilumos@gmail.com>
2017-11-12 21:41:16 +0000

    Moving Goldeneye Source files to new hosting

--- linux/GoldeneyeSource/install.sh
+++ linux/GoldeneyeSource/install.sh
@@ -4,6 +4,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 info "Downloading game server files"
 
-curl -L "http://files.gameservermanagers.com/GoldenEyeSource/GoldenEye_Source_v5.0.1_full_server_linux.tar.bz2" | tar -xj -C "$DIR/server"
+curl -L "https://zeropingheroes.ams3.digitaloceanspaces.com/GoldenEye_Source_v5.0.6_full_server_windows.7z" > "$DIR/server/GoldenEye_Source_v5.0.6_full_server_windows.7z"
+
+7z x "$DIR/server/GoldenEye_Source_v5.0.6_full_server_windows.7z" -o"$DIR/server"
+
+info "Removing temporary files"
+rm "$DIR/server/GoldenEye_Source_v5.0.6_full_server_windows.7z"
+
+info "Moving default server config"
+mv "$DIR/server/gesource/cfg/server.cfg" "$DIR/server/gesource/cfg/server-default.cfg"
 
 source $DIR/update.sh
