c718bb1c57506ddc304d5fd94bd26f421e3c35f7
ilumos <ilumos@gmail.com>
2017-11-12 21:47:14 +0000

    Preventing install/update scripts from being called directly

--- linux/GoldeneyeSource/install.sh
+++ linux/GoldeneyeSource/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -z $CALLED_FROM_MAIN_SCRIPT ] && { echo "ERROR: This script can only be called from ./game-servers"; exit 1; }
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 info "Downloading game server files"
--- linux/QuakeIIIArena/install.sh
+++ linux/QuakeIIIArena/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -z $CALLED_FROM_MAIN_SCRIPT ] && { echo "ERROR: This script can only be called from ./game-servers"; exit 1; }
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Extension is .tar.bz2 but the file is not bzipped
