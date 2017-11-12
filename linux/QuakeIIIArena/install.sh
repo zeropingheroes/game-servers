dc61eaf5c0c143aaf1232e06735ed9bc131484c6
ilumos <ilumos@gmail.com>
2017-11-12 20:40:21 +0000

    Moving Quake III Arena and UT2004 installers to new hosting location

--- linux/QuakeIIIArena/install.sh
+++ linux/QuakeIIIArena/install.sh
@@ -5,10 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Extension is .tar.bz2 but the file is not bzipped
 info "Downloading game server files"
 info "Downloading Quake III Arena"
-curl -L "http://files.gameservermanagers.com/Quake3/quake3-1.32c-x86-full-linux.tar.bz2" | tar -xz -C "$DIR/server"
+curl -L "https://zeropingheroes.ams3.digitaloceanspaces.com/quake3-1.32c-x86-full-linux.tar.bz2" | tar -xz -C "$DIR/server"
 
 info "Downloading OSP"
-curl -L "http://osp.dget.cc/orangesmoothie/downloads/osp-Quake3-1.03a_full.zip" > "$DIR/server/osp-Quake3-1.03a_full.zip"
+curl -L "https://zeropingheroes.ams3.digitaloceanspaces.com/osp-Quake3-1.03a_full.zip" > "$DIR/server/osp-Quake3-1.03a_full.zip"
 
 info "Extracting OSP"
 unzip -q "$DIR/server/osp-Quake3-1.03a_full.zip" -d "$DIR/server/"
--- linux/UT2004/install.sh
+++ linux/UT2004/install.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 info "Downloading game server files"
 
-curl -L "http://files.gameservermanagers.com/UnrealTournament2004/ut2004-server-3339-ultimate-linux.tar.bz2" | tar -xj -C "$DIR/server"
+curl -L "https://zeropingheroes.ams3.digitaloceanspaces.com/ut2004-server-3339-ultimate-linux.tar.bz2" | tar -xj -C "$DIR/server"
