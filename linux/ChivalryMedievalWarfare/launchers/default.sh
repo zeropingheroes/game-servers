7e77c4f9a5d330459364f1532d1cb40a5ef055d6
ilumos <ilumos@gmail.com>
2017-11-18 15:40:10 +0000

    Removing of cached config directory, so config changes take effect

--- linux/ChivalryMedievalWarfare/launchers/default.sh
+++ linux/ChivalryMedievalWarfare/launchers/default.sh
@@ -2,4 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Clear the cached config directory
+rm -r ~/.local/share/TornBanner
+
 cd $DIR/../server/Binaries/Linux && ./UDKGameServer-Linux aocffa-moor_p\?steamsockets\?port=7777\?queryport=27015 -configsubdir=ZPH -seekfreeloadingserver
--- linux/ChivalryMedievalWarfare/launchers/giant-slayers.sh
+++ linux/ChivalryMedievalWarfare/launchers/giant-slayers.sh
@@ -2,4 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Clear the cached config directory
+rm -r ~/.local/share/TornBanner
+
 cd $DIR/../server/Binaries/Linux && ./UDKGameServer-Linux AOCFFA-StoneshillVillage_P\?modname=giantslayers\?steamsockets\?port=7777\?queryport=27015 -configsubdir=ZPH -seekfreeloadingserver -sdkfileid=226726898
