f02cda99883aadd95a4662d355d608b797ca1d30
ilumos <ilumos@gmail.com>
2020-01-18 18:03:50 +0000

    Get Q3A files from Google Drive

--- linux/QuakeIIIArena/install.sh
+++ linux/QuakeIIIArena/install.sh
@@ -7,13 +7,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Extension is .tar.bz2 but the file is not bzipped
 info "Downloading game server files"
 info "Downloading Quake III Arena"
-curl -L "http://zeropingheroes.ams3.digitaloceanspaces.com/quake3-1.32c-x86-full-linux.tar.bz2" | tar -xz -C "$DIR/server"
+~/go/bin/goodls -u "https://drive.google.com/file/d/1u7hhlZihpruWe86Ae4Uqj0oUndyEy6M6/view"
+
+info "Extracting Quake III Arena"
+tar -xzf "quake3-1.32c-x86-full-linux.tar.bz2" -C "$DIR/server"
 
 info "Downloading OSP"
-curl -L "http://zeropingheroes.ams3.digitaloceanspaces.com/osp-Quake3-1.03a_full.zip" > "$DIR/server/osp-Quake3-1.03a_full.zip"
+~/go/bin/goodls -u "https://drive.google.com/file/d/1RrHzS_FpERtNDYxqflnijlT9gbmsEH5U/view"
 
 info "Extracting OSP"
 unzip -q "$DIR/server/osp-Quake3-1.03a_full.zip" -d "$DIR/server/"
 
 info "Removing temporary files"
-rm "$DIR/server/osp-Quake3-1.03a_full.zip"
+rm "$DIR/server/osp-Quake3-1.03a_full.zip" "$DIR/server/quake3-1.32c-x86-full-linux.tar.bz2"
