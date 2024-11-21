ebf555a438ba7e340f2df2700caa238e5af6eabe
ilumos <ilumos@gmail.com>
2024-11-21 23:34:39 +0000

    Use system steamcmd

--- linux/SteamCMD/download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-URL="http://media.steampowered.com/client/steamcmd_linux.tar.gz"
-
-cd $DIR && /usr/bin/curl -sqL $URL | tar zxvf -
