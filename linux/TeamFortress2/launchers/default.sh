f0576907e03afbe57311ff66a0f5e1eecba342ec
ilumos <ilumos@gmail.com>
2025-01-04 22:52:18 +0000

    TF2: Remove unnecessary parameters

--- linux/TeamFortress2/launchers/default.sh
+++ linux/TeamFortress2/launchers/default.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR/../ && server/srcds_run -console -game tf +exec server-zph.cfg +sv_lan 1 +map koth_sawmill +maxplayers 24
+cd $DIR/../ && server/srcds_run -game tf +sv_lan 1 +exec server-zph.cfg
