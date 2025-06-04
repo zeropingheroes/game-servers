5a496b9b3a1cbaa7641821d21ca7f309d2523ba5
ilumos <ilumos@gmail.com>
2025-06-04 13:21:36 +0100

    Split OSP and vanilla configs. Add launchers

---
+++ linux/QuakeIIIArena/launchers/osp-ffa-then-ctf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR/../server && ./q3ded +set fs_homepath "$DIR/../server" +set fs_game osp +set dedicated 1 +set net_port 27960 +exec "zph-vanilla-ffa-then-ctf.cfg"
