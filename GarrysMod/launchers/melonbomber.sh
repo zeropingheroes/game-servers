394799b44e1e84a818d04216ccd7d80ca1ad8698
ilumos <ilumos@gmail.com>
2025-06-23 21:26:32 +0000

    GMod: Update PropHunt & Melonbomber

--- GarrysMod/launchers/melonbomber.sh
+++ GarrysMod/launchers/melonbomber.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd $DIR/../ && server/srcds_run -console -authkey EC5F82F81D15A10CBC71F8B87F180F9A +host_workshop_collection 483705780 +exec server-zph-melonbomber.cfg -game garrysmod +sv_lan 1 -maxplayers 32 +gamemode melonbomber +map mb_melonbomber 
+cd $DIR/../ && server/srcds_run -console -game garrysmod +sv_lan 1 -maxplayers 32 +host_workshop_collection 483705780 +gamemode melonbomber +exec zph-melonbomber.cfg +map mb_melonbomber
--- GarrysMod/launchers/prophunt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cd $DIR/../ && server/srcds_run -console -authkey EC5F82F81D15A10CBC71F8B87F180F9A +host_workshop_collection 481591406 +exec server-zph-prophunt.cfg -game garrysmod +sv_lan 1 -maxplayers 32 +gamemode prop_hunt +map ph_westerncity
