f62f96f96b06bff46e1a42d08a787f16e231a49a
ilumos <ilumos@gmail.com>
2025-07-01 21:31:48 +0000

    Update Chivalry Medieval Warfare

--- ChivalryMedievalWarfare/launchers/default.sh
+++ ChivalryMedievalWarfare/launchers/default.sh
@@ -2,7 +2,20 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Clear the cached config directory
-rm -r ~/.local/share/TornBanner
+source "$DIR/../../functions.sh"
 
-cd $DIR/../server/Binaries/Linux && ./UDKGameServer-Linux aocffa-moor_p\?steamsockets\?port=7777\?queryport=27015 -configsubdir=ZPH -seekfreeloadingserver
+echo
+echo_info "The server will echo these errors, and not accept any input:"
+echo
+echo "[S_API FAIL] SteamAPI_Init() failed; SteamAPI_IsSteamRunning() failed."
+echo "IPC function call IClientUtils::GetSteamRealm took too long"
+echo "ERROR: PhysXUpdateLoader.so: cannot open shared object file: No such file or directory"
+echo
+echo_info "However, the server is actually running OK!"
+echo
+sleep 4
+
+# Clear the cached config directory
+rm -rf ~/.local/share/TornBanner
+
+cd $DIR/../server/Binaries/Linux && ./UDKGameServer-Linux "aocffa-moor_p ?steamsockets -port=7777 -queryport=27015 -seekfreeloadingserver -configsubdir=ZPH"
