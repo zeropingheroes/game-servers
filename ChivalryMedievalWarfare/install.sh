dd55626417b7c909c654ada2bcfa666cfc113249
ilumos <ilumos@gmail.com>
2025-07-02 21:28:22 +0000

    Chivalry: set admin password and print instructions

--- ChivalryMedievalWarfare/install.sh
+++ ChivalryMedievalWarfare/install.sh
@@ -3,6 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source $DIR/../functions.sh
+source $DIR/../.env
 
 echo_info "Downloading game server files"
 source $DIR/update.sh
@@ -13,3 +14,5 @@ cp $DIR/../SteamCMD/linux32/steamclient.so $DIR/server/Binaries/Linux/lib
 echo_info "Creating steam_appid.txt"
 echo 219640 > $DIR/server/Binaries/Linux/steam_appid.txt
 
+echo_info "Setting admin password"
+sed -i "s/^AdminPassword=.*/AdminPassword=$RCON_PASSWORD/" "$DIR/configs/UDKGame/Config/ZPH/PCServer-UDKGame.ini"
--- ChivalryMedievalWarfare/launchers/default.sh
+++ ChivalryMedievalWarfare/launchers/default.sh
@@ -13,7 +13,41 @@ echo "ERROR: PhysXUpdateLoader.so: cannot open shared object file: No such file 
 echo
 echo_info "However, the server is actually running OK!"
 echo
-sleep 4
+echo_info "To control the server:"
+echo_info "1. Connect as a player"
+echo_info "2. Press the apostrophe (') key"
+echo_info "3. Type \"adminlogin <your password>\""
+echo ""
+echo_info "Then use the following commands:"
+echo ""
+echo "Admin MapList                               List maps available on the server"
+echo "AdminChangeMap <map name>                   Change map"
+echo "AdminRestartMap                             Restart map"
+echo "Admin GetInfo                               List connected players"
+echo "AdminChangeTeam <player name>               Swap player to opposite team"
+echo "AdminKick <player name>                     Kick player from server"
+echo "AdminKickBan <player name>                  Kick and ban player from server"
+echo "AdminChangeTeamDamageAmount <0.0 to 1.0>    Set team damage multiplier"
+echo "AdminCancelVote                             Cancel current vote"
+echo "Admin Aoc_EndGame                           End current map"
+echo "Admin AddBots <number>                      Add bots"
+echo "Admin Killbots                              Kick all bots"
+echo "Admin Aoc_ShowDamage 1                      Print damage to console"
+echo "AdminForceVoiceMute <player name>           Mute player voice chat"
+echo "AdminForceVoiceUnmute <player name>         Unmute player voice chat"
+echo "AdminForceTextMute <player name>            Mute player text chat"
+echo "AdminForceTextUnmute <player name>          Unmute player text chat"
+echo "Admin Aoc_Slowmo <0.001 to 2.0>             Set game speed multiplier"
+echo "Admin Pause                                 Pause server (if enabled)"
+echo "AdminLogout                                 Log out of admin"
+echo ""
+echo_info "You must log in again after each map change."
+echo_info "For convenience, bind a key to logging in as admin, for example:"
+echo ""
+echo_info "Setbind O AdminLogin <your password>"
+echo
+echo "---------------------------------------------------------------------------------------"
+echo
 
 # Clear the cached config directory
 rm -rf ~/.local/share/TornBanner
